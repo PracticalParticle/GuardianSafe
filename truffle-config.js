15b6bd40293f90c2c2f54f2c66b15b4800d1a358
JaCoderX <jaco1234h@gmail.com>
2025-09-28 23:54:07 +0300

    use shanghai evm version with 0.8.25

--- truffle-config.js
+++ truffle-config.js
@@ -102,14 +102,14 @@ module.exports = {
   // Configure your compilers
   compilers: {
     solc: {
-      version: "0.8.25",    // Fetch exact version from solc-bin (default: truffle's version)
+      version: "0.8.25",
       // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
       settings: {          // See the solidity docs for advice about optimization and evmVersion
        optimizer: {
          enabled: true,
          runs: 1
        },
-      //  evmVersion: "byzantium"
+       evmVersion: "shanghai"  // Minimum version that supports chainid and OpenZeppelin
       }
     }
   },
